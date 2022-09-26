6d50cd91bb041e061ea083a2af253d1dde32bb30
SigmaAjax <jankennny@seznam.cz>
2022-09-26 17:42:09 +0200

    Form for adding new staff to the school

---
+++ src/admin/adminComponents/FormStep.js
@@ -0,0 +1,58 @@
+import FormStepInput from './FormStepInput.js';
+
+export default function FormStep({values, onChange, dataStep}) {
+	const {headline, ...rest} = values;
+	const leftValues = Object.values(rest);
+
+	return (
+		<div className={'card'} data-step={dataStep}>
+			<h3 className="basic-info step-title">
+				{headline.charAt(0).toUpperCase() + headline.slice(1)}
+			</h3>
+			{leftValues.map((key) => {
+				if (key.name === 'titul') {
+					return (
+						<div key={key.name} className="form-group">
+							<label htmlFor={key.name} id={key.name}>
+								{key.headline.charAt(0).toUpperCase() + key.headline.slice(1)}
+							</label>
+							<select name={key.name} onChange={onChange}>
+								<option key={'none'} value={null}>
+									Žádný
+								</option>
+								<option key={'bc'} value="bc.">
+									Bc.
+								</option>
+								<option key={'mgr'} value="mgr.">
+									Mgr.
+								</option>
+								<option key={'phdr'} value="phdr.">
+									PhDr.
+								</option>
+							</select>
+						</div>
+					);
+				} else if (key.pattern) {
+					return (
+						<FormStepInput
+							required={true}
+							key={key.name}
+							pattern={key.pattern}
+							values={key}
+							onChange={onChange}
+						/>
+					);
+				} else {
+					return (
+						<FormStepInput
+							required={true}
+							key={key.name}
+							values={key}
+							onChange={onChange}
+						/>
+					);
+				}
+			})}
+		</div>
+	);
+}
