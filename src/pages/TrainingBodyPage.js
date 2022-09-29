4fe7e518fee96fde27cfc8c66485066aefcece23
SigmaAjax <jankennny@seznam.cz>
2022-09-29 14:11:50 +0200

    Revert "Form for adding new staff to the school"
    This reverts commit 6d50cd91bb041e061ea083a2af253d1dde32bb30.

--- src/pages/TrainingBodyPage.js
+++ src/pages/TrainingBodyPage.js
@@ -1,5 +1,9 @@
-import EmployeeForm from '../admin/EmployeeForm';
-
 export default function TrainingBodyPage() {
-	return <EmployeeForm />;
+	return (
+		<div className="container">
+			<div className="item one">flex item 1</div>
+			<div className="item two">flex item 2</div>
+			<div className="item three">flex item 3</div>
+		</div>
+	);
 }
