b5da1bb650c96aa66ca9a185cf77f8f198d52d22
SigmaAjax <jankennny@seznam.cz>
2022-11-12 19:08:08 +0100

    Dropzone package added

--- client/src/admin/adminComponents/GalleryComponents/CreateAlbum.js
+++ client/src/admin/adminComponents/GalleryComponents/CreateAlbum.js
@@ -1,4 +1,18 @@
+import {useCallback} from 'react';
+import {useDropzone} from 'react-dropzone';
+
 export default function CreateAlbum() {
+	const onDrop = useCallback((acceptedFiles, rejectedFiles) => {
+		console.log('accepted files', acceptedFiles);
+		console.log('rejected files', rejectedFiles);
+	}, []);
+	const {getRootProps, getInputProps, isDragActive} = useDropzone({
+		onDrop,
+		accept: 'image/*',
+	});
+
+	//console.log(getRootProps(), getInputProps());
+
 	return (
 		<div className="item three">
 			<form className="form-group" onSubmit={console.log('Album created')}>
@@ -17,6 +31,12 @@ export default function CreateAlbum() {
 					type="text"
 					placeholder="Byli jsme na výletě...."
 				></textarea>
+				<div className="dropzone" {...getRootProps()}>
+					<input {...getInputProps()} />
+					{isDragActive
+						? 'Chystáte se vložit fotografie'
+						: 'Můžete vložit fotografie'}
+				</div>
 				<button type="submit">Vytvořit Album</button>
 			</form>
 		</div>
